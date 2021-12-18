f933526b02302f1cf8e2c84e7d352e8261eafcbb
murugeshan.kn <murugeshan.kn@ajax-engg.com>
2021-12-18 20:22:07 +0530

    No Change  But Commiting

--- DealerManagementSystem/Location.aspx.cs
+++ DealerManagementSystem/Location.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace DealerManagementSystem
 {
-    public partial class Location : System.Web.UI.Page
+    public partial class Location : System.Web.UI.Page 
     {
         protected void Page_Load(object sender, EventArgs e)
         {
