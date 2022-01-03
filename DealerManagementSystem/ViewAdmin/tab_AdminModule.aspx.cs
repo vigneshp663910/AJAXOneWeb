ec891faace33713e43339893b02e13eed9ebce67
murugeshan.kn <murugeshan.kn@ajax-engg.com>
2022-01-03 12:49:34 +0530

    Module Admin Pages Introduced

---
+++ DealerManagementSystem/ViewAdmin/tab_AdminModule.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DealerManagementSystem.ViewAdmin
+{
+    public partial class tab_AdminModule : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Page.ClientScript.RegisterStartupScript(this.GetType(), "Script1", "<script type='text/javascript'>SetScreenTitle('Admin Module');</script>");
+
+        }
+    }
+}
---
+++ DealerManagementSystem/ViewAdmin/tab_AdminModulePages.aspx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DealerManagementSystem.ViewAdmin
+{
+    public partial class tab_AdminModulePages : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
