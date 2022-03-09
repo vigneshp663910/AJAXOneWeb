5e5e4fa00117edca71fdcab69ddf4564ec1d1cb0
murugeshan.kn <murugeshan.kn@ajax-engg.com>
2022-03-09 09:42:10 +0530

    Screen Title Set

--- DealerManagementSystem/ViewMaster/Application.aspx.cs
+++ DealerManagementSystem/ViewMaster/Application.aspx.cs
@@ -14,6 +14,7 @@ namespace DealerManagementSystem.ViewMaster
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Page.ClientScript.RegisterStartupScript(this.GetType(), "Script1", "<script type='text/javascript'>SetScreenTitle('Master » Application');</script>");
             if (!IsPostBack)
             {
                 try
