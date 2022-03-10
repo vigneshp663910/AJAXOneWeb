0ba27e3e0f602008a8d5d821c7b8851e61272f9b
murugeshan.kn <murugeshan.kn@ajax-engg.com>
2022-03-10 16:44:41 +0530

    Signout Confirmation Page Introduced...

---
+++ DealerManagementSystem/Account/SignOut.aspx.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DealerManagementSystem.Account
+{
+    public partial class SignOut : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogOutYes_Click(object sender, EventArgs e)
+        {
+            Session.Abandon();
+            Request.Cookies.Clear();
+            Session.Clear();
+            Session.RemoveAll();
+            Response.Redirect("/Account/SignOut.htm");
+            
+        }
+
+        protected void btnLogOutNo_Click(object sender, EventArgs e)
+        {
+        
+            Response.Redirect("../Home.aspx");
+        }
+    }
+}
