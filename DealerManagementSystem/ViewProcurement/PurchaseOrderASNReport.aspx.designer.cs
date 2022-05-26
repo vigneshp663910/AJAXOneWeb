370a71b0f227ca8d91bb186e6e1609957165a1c8
vigneshajax <vignesh.p@ajax-engg.com>
2022-05-26 11:14:50 +0530

    PurchaseorderASNReport Changes

--- DealerManagementSystem/ViewProcurement/PurchaseOrderASNReport.aspx.designer.cs
+++ DealerManagementSystem/ViewProcurement/PurchaseOrderASNReport.aspx.designer.cs
@@ -265,14 +265,5 @@ namespace DealerManagementSystem.ViewProcurement
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.GridView gvDCM;
-
-        /// <summary>
-        /// Panel2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Panel Panel2;
     }
 }
