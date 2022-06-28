5896f6f7192156813c4e7fad76a7a76561557e74
john-ajax <john.peter@ajax-engg.com>
2022-06-28 14:13:51 +0530

    Enquiry

---
+++ Properties/PEnquiry.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    public class PEnquiry
+    {
+        public long EnquiryID { get; set; }
+        public DateTime EnquiryDate { get; set; }
+        public string EnquiryNumber { get; set; }
+        public string CustomerName { get; set; }
+        public string PersonName { get; set; }
+        public string Mail { get; set; }
+        public string Mobile { get; set; }
+        public string Address { get; set; }
+        public PLeadSource Source { get; set; }
+        public PPreSaleStatus Status { get; set; }
+        public PDMS_Country Country { get; set; }
+        public PDMS_State State { get; set; }
+        public PDMS_District District { get; set; }
+        public string Product { get; set; }
+        public string Remarks { get; set; } 
+        public PUser CreatedBy { get; set; }
+        public string CreatedOn { get; set; } 
+    }
+}
