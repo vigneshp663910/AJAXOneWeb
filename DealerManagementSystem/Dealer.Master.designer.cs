93474fcd98c3572f9ea6622c03ad99c45b5dab27
murugeshan-kn <murugeshan.kn@ajax-engg.com>
2023-12-22 16:13:58 +0530

    Dealer Master Little Correction

--- DealerManagementSystem/Dealer.Master.designer.cs
+++ DealerManagementSystem/Dealer.Master.designer.cs
@@ -201,7 +201,7 @@ namespace DealerManagementSystem
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlGenericControl ICHomeWithTiles;
+        //protected global::System.Web.UI.HtmlControls.HtmlGenericControl ICHomeWithTiles;
 
         /// <summary>
         /// ICAppoinment control.
