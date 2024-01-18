1de9cbf6406a36b30e696f9c5c6253c06bbd18b4
murugeshan-kn <murugeshan.kn@ajax-engg.com>
2024-01-18 17:41:15 +0530

    Page Addedd to Access e-Catalogue

---
+++ DealerManagementSystem/RedirectTo/eCatalogueN.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Dealer.Master" AutoEventWireup="true" CodeBehind="eCatalogueN.aspx.cs" Inherits="DealerManagementSystem.RedirectTo.eCatalogueN" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+    
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+   <%-- <iframe id="iframeProfile" src="https://ajaxapps.ajax-engg.com:8095/eCatalogue/SP_Catalogue?ProductGroup=XX&Model=X&McNo=XXXXXXXXXXXX" width="100%" style="height: 98vh; margin-top:-70px" frameborder="0"></iframe>--%>
+     <iframe id="iframeProfile" src="https://ajaxapps.ajax-engg.com:8095/eCatalogue/SignIn.aspx" width="100%" style="height: 98vh; margin-top:-70px" frameborder="0"></iframe>
+
+</asp:Content>
---
+++ DealerManagementSystem/RedirectTo/eCatalogueN.aspx.cs
@@ -0,0 +1,30 @@
+﻿
+
+using Business;
+using Properties;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DealerManagementSystem.RedirectTo
+{
+    public partial class eCatalogueN : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+            if (!IsPostBack)
+            {
+                Page.ClientScript.RegisterStartupScript(this.GetType(), "Script1", "<script type='text/javascript'>SetScreenTitle('e-Catalogue » eSpare Parts Catalogue');</script>");
+                
+            }
+
+            //Response.Redirect("https://ajaxapps.ajax-engg.com:8095/eCatalogue/Home");
+        }
+    }
+}
