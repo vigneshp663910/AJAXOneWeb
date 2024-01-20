944687c7c9a87d70f2f3ee19b56ba849d0c7fca9
john-ajax <john.peter@ajax-engg.com>
2024-01-20 11:22:20 +0530

    Dealer Business Excellence Report

--- DealerManagementSystem/ViewPreSale/Planning/DealerBusinessExcellence.aspx.designer.cs
+++ DealerManagementSystem/ViewPreSale/Planning/DealerBusinessExcellence.aspx.designer.cs
@@ -114,24 +114,6 @@ namespace DealerManagementSystem.ViewPreSale.Planning
         protected global::System.Web.UI.WebControls.Label lblRowCountV;
 
         /// <summary>
-        /// ibtnVTArrowLeft control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ImageButton ibtnVTArrowLeft;
-
-        /// <summary>
-        /// ibtnVTArrowRight control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ImageButton ibtnVTArrowRight;
-
-        /// <summary>
         /// gvMissionPlanning control.
         /// </summary>
         /// <remarks>
