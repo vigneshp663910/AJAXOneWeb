2a3e979b55c31b4c48d9274e3f5e220fd824a21c
john-ajax <john.peter@ajax-engg.com>
2024-05-27 12:04:22 +0530

    BDMS_Material For Parts App

--- Business/BDMS_Material.cs
+++ Business/BDMS_Material.cs
@@ -20,8 +20,6 @@ namespace Business
         {
             provider = new ProviderFactory().GetProvider();
         }
-
-
         public List<string> GetMaterialAutocomplete(string Material, string MaterialType, int? DivisionID)
         {
             List<string> Materials = new List<string>();
@@ -59,21 +57,21 @@ namespace Business
             { }
             return Materials;
         }
-        public List<PDMS_Material> GetMaterialAutocompleteN(string Material, string MaterialType, int? DivisionID)
+        public List<PDMS_Material> GetMaterialAutocompleteN(string Material, string MaterialType, int? DivisionID, string WithDotFLMaterial = "true")
         {
             TraceLogger.Log(DateTime.Now);
             string endPoint = "";
             if (MaterialType == "DIEN")
             {
-                endPoint = "Material/ServiceMaterialAutocomplete?Material=" + Material + "&DivisionID=" + DivisionID;
+                endPoint = "Material/ServiceMaterialAutocomplete?Material=" + Material + "&DivisionID=" + DivisionID + "&WithDotFLMaterial=" + WithDotFLMaterial;
             }
             else if (MaterialType == "FERT")
             {
-                endPoint = "Material/MaterialFinishedGoodsAutocomplete?Material=" + Material + "&MaterialType=" + MaterialType + "&DivisionID=" + DivisionID;
+                endPoint = "Material/MaterialFinishedGoodsAutocomplete?Material=" + Material + "&MaterialType=" + MaterialType + "&DivisionID=" + DivisionID + "&WithDotFLMaterial=" + WithDotFLMaterial;
             }
             else
             {
-                endPoint = "Material/MaterialAccessoriesAutocomplete?Material=" + Material + "&MaterialType=" + MaterialType + "&DivisionID=" + DivisionID;
+                endPoint = "Material/MaterialAccessoriesAutocomplete?Material=" + Material + "&MaterialType=" + MaterialType + "&DivisionID=" + DivisionID + "&WithDotFLMaterial=" + WithDotFLMaterial;
             }
             return JsonConvert.DeserializeObject<List<PDMS_Material>>(JsonConvert.SerializeObject(JsonConvert.DeserializeObject<PApiResult>(new BAPI().ApiGet(endPoint)).Data));
         }
@@ -163,50 +161,140 @@ namespace Business
         {
             string endPoint = "Material/Material?MaterialID=" + MaterialID + "&MaterialCode=" + MaterialCode
                 + "&DivisionID=" + DivisionID + "&ModelID=" + ModelID + "&IsActive=" + IsActive;
-            return JsonConvert.DeserializeObject<List<PDMS_Material>>(JsonConvert.SerializeObject(JsonConvert.DeserializeObject<PApiResult>(new BAPI().ApiGet(endPoint)).Data));
+            DataTable dt = JsonConvert.DeserializeObject<DataTable>(JsonConvert.SerializeObject(JsonConvert.DeserializeObject<PApiResult>(new BAPI().ApiGet(endPoint)).Data));
+            List<PDMS_Material> SOIs = new List<PDMS_Material>();
+            PDMS_Material SOI = new PDMS_Material();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                SOI = new PDMS_Material();
+                SOI.MaterialID = Convert.ToInt32(dr["MaterialID"]);
+                SOI.MaterialCode = Convert.ToString(dr["MaterialCode"]);
+                SOI.MaterialDescription = Convert.ToString(dr["MaterialDescription"]);
+                SOI.BaseUnit = Convert.ToString(dr["BaseUnit"]);
+                SOI.MaterialType = Convert.ToString(dr["MaterialType"]);
+                SOI.MaterialGroup = Convert.ToString(dr["MaterialGroup"]);
+                SOI.WeightUnit = Convert.ToString(dr["WeightUnit"]);
+                //  SOI.MaterialDivision = Convert.ToString(dr["MaterialDivision"]);
+                SOI.GrossWeight = DBNull.Value == dr["GrossWeight"] ? 0 : Convert.ToDecimal(dr["GrossWeight"]);
+                SOI.NetWeight = DBNull.Value == dr["NetWeight"] ? 0 : Convert.ToDecimal(dr["NetWeight"]);
+                SOI.HSN = Convert.ToString(dr["HSNCode"]);
+                SOI.TaxPercentage = DBNull.Value == dr["TaxPercentage"] ? 0 : Convert.ToDecimal(dr["TaxPercentage"]);
+                SOI.CurrentPrice = DBNull.Value == dr["CurrentPrice"] ? 0 : Convert.ToDecimal(dr["CurrentPrice"]);
+                SOI.IsMainServiceMaterial = DBNull.Value == dr["IsMainServiceMaterial"] ? false : Convert.ToBoolean(dr["IsMainServiceMaterial"]);
+                SOI.Model = new PDMS_Model()
+                {
+                    Model = Convert.ToString(dr["Model"]),
+                    ModelCode = Convert.ToString(dr["ModelCode"]),
+                    ModelDescription = Convert.ToString(dr["ModelDescription"]),
+                    Division = new PDMS_Division()
+                    {
+                        DivisionID = Convert.ToInt32(dr["DivisionID"]),
+                        DivisionCode = Convert.ToString(dr["DivisionCode"]),
+                        DivisionDescription = Convert.ToString(dr["DivisionDescription"]),
+
+                        WarrantyDeliveryHours = Convert.ToString(dr["WarrantyDeliveryHours"]),
+                        UOM = Convert.ToString(dr["UOM"])
+                    }
+                };
+                SOI.IsActive = Convert.ToBoolean(dr["IsActive"]);
+                SOIs.Add(SOI);
+            }
+            return SOIs;
+            // return JsonConvert.DeserializeObject<List<PDMS_Material>>(JsonConvert.SerializeObject(JsonConvert.DeserializeObject<PApiResult>(new BAPI().ApiGet(endPoint)).Data));
 
         }
 
         public List<PDMS_Material> GetMaterialSupersede(int? MaterialID, string MaterialCode)
         {
-            TraceLogger.Log(DateTime.Now);
-            List<PDMS_Material> SOIs = new List<PDMS_Material>();
-            PDMS_Material SOI = new PDMS_Material();
-            try
-            {
-                DbParameter MaterialIDP = provider.CreateParameter("MaterialID", MaterialID, DbType.Int32);
-                DbParameter MaterialCodeP = provider.CreateParameter("MaterialCode", string.IsNullOrEmpty(MaterialCode) ? null : MaterialCode, DbType.String);
-                DbParameter[] Params = new DbParameter[2] { MaterialIDP, MaterialCodeP };
+            string endPoint = "Material/GetMaterialSupersede?MaterialID=" + MaterialID + "&MaterialCode=" + MaterialCode;
+            return JsonConvert.DeserializeObject<List<PDMS_Material>>(JsonConvert.SerializeObject(JsonConvert.DeserializeObject<PApiResult>(new BAPI().ApiGet(endPoint)).Data));
+            //TraceLogger.Log(DateTime.Now);
+            //List<PDMS_Material> SOIs = new List<PDMS_Material>();
+            //PDMS_Material SOI = new PDMS_Material();
+            //try
+            //{
+            //    DbParameter MaterialIDP = provider.CreateParameter("MaterialID", MaterialID, DbType.Int32);
+            //    DbParameter MaterialCodeP = provider.CreateParameter("MaterialCode", string.IsNullOrEmpty(MaterialCode) ? null : MaterialCode, DbType.String);
+            //    DbParameter[] Params = new DbParameter[2] { MaterialIDP, MaterialCodeP };
 
-                using (DataSet DataSet = provider.Select("GetMaterialSupersede", Params))
-                {
-                    if (DataSet != null)
-                    {
-                        foreach (DataRow dr in DataSet.Tables[0].Rows)
-                        {
+            //    using (DataSet DataSet = provider.Select("GetMaterialSupersede", Params))
+            //    {
+            //        if (DataSet != null)
+            //        {
+            //            foreach (DataRow dr in DataSet.Tables[0].Rows)
+            //            {
 
-                            SOI = new PDMS_Material();
-                            SOI.MaterialCode = Convert.ToString(dr["MaterialCode"]);
-                            SOI.MaterialDescription = Convert.ToString(dr["MaterialDescription"]);
-                            SOI.Supersede = new PSupersede();
-                            SOI.Supersede.Material = Convert.ToString(dr["SupersedeCode"]);
-                            SOI.Supersede.MaterialDescription = Convert.ToString(dr["SupersedeMaterailDescription"]);
-                            SOI.Supersede.ValidFrom = Convert.ToDateTime(dr["ValidFrom"]);
-                            SOI.Supersede.ValidTo = Convert.ToDateTime(dr["ValidTo"]);
-                            SOI.Supersede.Description = Convert.ToString(dr["SupersedeRemarks"]);
-                            SOIs.Add(SOI);
-                        }
-                        return SOIs;
-                        TraceLogger.Log(DateTime.Now);
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                new FileLogger().LogMessage("BDMS_Material", "GetMaterialSupersede", ex);
-                throw ex;
-            }
-            return SOIs;
+            //                SOI = new PDMS_Material();
+            //                SOI.MaterialID = Convert.ToInt32(dr["MaterialID"]);
+            //                SOI.MaterialCode = Convert.ToString(dr["MaterialCode"]);
+            //                SOI.MaterialDescription = Convert.ToString(dr["MaterialDescription"]);
+            //                SOI.Supersede = new PSupersede();
+            //                SOI.Supersede.MaterialID = Convert.ToInt32(dr["SupersedeMaterialID"]);
+            //                SOI.Supersede.Material = Convert.ToString(dr["SupersedeCode"]);
+            //                SOI.Supersede.MaterialDescription = Convert.ToString(dr["SupersedeMaterailDescription"]);
+            //                SOI.Supersede.ValidFrom = Convert.ToDateTime(dr["ValidFrom"]);
+            //                SOI.Supersede.ValidTo = Convert.ToDateTime(dr["ValidTo"]);
+            //                SOI.Supersede.Description = Convert.ToString(dr["SupersedeRemarks"]);
+            //                SOIs.Add(SOI);
+            //            }
+            //            return SOIs;
+            //            TraceLogger.Log(DateTime.Now);
+            //        }
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    new FileLogger().LogMessage("BDMS_Material", "GetMaterialSupersede", ex);
+            //    throw ex;
+            //}
+            //return SOIs;
+        }
+        public int GetMaterialSupersedeFinalByID(int MaterialID)
+        {
+            string endPoint = "Material/GetMaterialSupersedeFinalByID?MaterialID=" + MaterialID;
+            return JsonConvert.DeserializeObject<int>(JsonConvert.SerializeObject(JsonConvert.DeserializeObject<PApiResult>(new BAPI().ApiGet(endPoint)).Data));
+
+            //int cout = 0;
+            //int smaterial = MaterialID;
+            //do
+            //{
+            //    cout = cout + 1;
+            //    if(cout==100)
+            //    {
+            //        throw new Exception("Material Has supersede issue Please contact support team.");
+            //    }
+            //    MaterialID = smaterial;
+            //    List<PDMS_Material> d = GetMaterialSupersede(MaterialID, null);
+            //    if (d.Count == 1)
+            //    {
+            //        smaterial = d[0].Supersede.MaterialID;
+            //    }
+            //} while (smaterial != MaterialID);
+            //return MaterialID;
+        }
+        public string GetMaterialSupersedeFinalByCode(string MaterialCode)
+        {
+            string endPoint = "Material/GetMaterialSupersedeFinalByCode?MaterialCode=" + MaterialCode;
+            return JsonConvert.DeserializeObject<string>(JsonConvert.SerializeObject(JsonConvert.DeserializeObject<PApiResult>(new BAPI().ApiGet(endPoint)).Data));
+            //int cout = 0;
+            //string smaterial = MaterialCode;
+            //do
+            //{
+            //    cout = cout + 1;
+            //    if (cout == 100)
+            //    {
+            //        throw new Exception("Material Has supersede issue Please contact support team.");
+            //    }
+
+            //    MaterialCode = smaterial;
+            //    List<PDMS_Material> d = GetMaterialSupersede(null, MaterialCode);
+            //    if (d.Count == 1)
+            //    {
+            //        smaterial = d[0].Supersede.Material;
+            //    }
+            //} while (smaterial != MaterialCode);
+            //return MaterialCode;
         }
         public PMaterial MaterialPriceFromSap(string Customer, string Vendor, string OrderType, int Item, string Material, decimal Quantity, string IV_SEC_SALES, string PriceDate, string IsWarrenty)
         {
@@ -214,6 +302,103 @@ namespace Business
                 + "&Material=" + Material + "&Quantity=" + Quantity + "&IV_SEC_SALES=" + IV_SEC_SALES + "&PriceDate=" + PriceDate + "&IsWarrenty=" + IsWarrenty;
             return JsonConvert.DeserializeObject<PMaterial>(JsonConvert.SerializeObject(JsonConvert.DeserializeObject<PApiResult>(new BAPI().ApiGet(endPoint)).Data));
         }
+        //public List<PMaterial> MaterialPriceFromSapMulti(PMaterialTax_Api MaterialTax_Sap)
+        //{
+        //    string endPoint = "Material/MaterialPriceFromSapMulti";
+
+        //    PApiResult Result = JsonConvert.DeserializeObject<PApiResult>(new BAPI().ApiPut(endPoint, MaterialTax_Sap));
+        //    if (Result.Status == PApplication.Failure)
+        //    {
+        //        throw new Exception(Result.Message);
+        //    }
+        //    return JsonConvert.DeserializeObject<List<PMaterial>>(JsonConvert.SerializeObject(Result.Data));
+        //}
+
+        //public List<PMaterial> MaterialPriceFromSapApi(PSapMatPrice_Input MaterialPrice)
+        //{
+        //    string endPoint = "Material/MaterialPriceFromSapApi";
+
+        //    PApiResult Result = JsonConvert.DeserializeObject<PApiResult>(new BAPI().ApiPut(endPoint, MaterialPrice));
+        //    if (Result.Status == PApplication.Failure)
+        //    {
+        //        throw new Exception(Result.Message);
+        //    }
+        //    return JsonConvert.DeserializeObject<List<PMaterial>>(JsonConvert.SerializeObject(Result.Data));
+        //}
+
+
+        // **********************
+         
+        //public List<PDMS_Material> GetMaterialAutocompleteN(string Material, string MaterialType, int? DivisionID)
+        //{
+        //    TraceLogger.Log(DateTime.Now);
+        //    string endPoint = "";
+        //    if (MaterialType == "DIEN")
+        //    {
+        //        endPoint = "Material/ServiceMaterialAutocomplete?Material=" + Material + "&DivisionID=" + DivisionID;
+        //    }
+        //    else if (MaterialType == "FERT")
+        //    {
+        //        endPoint = "Material/MaterialFinishedGoodsAutocomplete?Material=" + Material + "&MaterialType=" + MaterialType + "&DivisionID=" + DivisionID;
+        //    }
+        //    else
+        //    {
+        //        endPoint = "Material/MaterialAccessoriesAutocomplete?Material=" + Material + "&MaterialType=" + MaterialType + "&DivisionID=" + DivisionID;
+        //    }
+        //    return JsonConvert.DeserializeObject<List<PDMS_Material>>(JsonConvert.SerializeObject(JsonConvert.DeserializeObject<PApiResult>(new BAPI().ApiGet(endPoint)).Data));
+        //}
+                
+
+        //public List<PDMS_Material> GetMaterialListSQL(int? MaterialID, string MaterialCode, int? DivisionID, int? ModelID, string IsActive)
+        //{
+        //    string endPoint = "Material/Material?MaterialID=" + MaterialID + "&MaterialCode=" + MaterialCode
+        //        + "&DivisionID=" + DivisionID + "&ModelID=" + ModelID + "&IsActive=" + IsActive;
+        //    return JsonConvert.DeserializeObject<List<PDMS_Material>>(JsonConvert.SerializeObject(JsonConvert.DeserializeObject<PApiResult>(new BAPI().ApiGet(endPoint)).Data));
+
+        //}
+
+        //public List<PDMS_Material> GetMaterialSupersede(int? MaterialID, string MaterialCode)
+        //{
+        //    TraceLogger.Log(DateTime.Now);
+        //    List<PDMS_Material> SOIs = new List<PDMS_Material>();
+        //    PDMS_Material SOI = new PDMS_Material();
+        //    try
+        //    {
+        //        DbParameter MaterialIDP = provider.CreateParameter("MaterialID", MaterialID, DbType.Int32);
+        //        DbParameter MaterialCodeP = provider.CreateParameter("MaterialCode", string.IsNullOrEmpty(MaterialCode) ? null : MaterialCode, DbType.String);
+        //        DbParameter[] Params = new DbParameter[2] { MaterialIDP, MaterialCodeP };
+
+        //        using (DataSet DataSet = provider.Select("GetMaterialSupersede", Params))
+        //        {
+        //            if (DataSet != null)
+        //            {
+        //                foreach (DataRow dr in DataSet.Tables[0].Rows)
+        //                {
+
+        //                    SOI = new PDMS_Material();
+        //                    SOI.MaterialCode = Convert.ToString(dr["MaterialCode"]);
+        //                    SOI.MaterialDescription = Convert.ToString(dr["MaterialDescription"]);
+        //                    SOI.Supersede = new PSupersede();
+        //                    SOI.Supersede.Material = Convert.ToString(dr["SupersedeCode"]);
+        //                    SOI.Supersede.MaterialDescription = Convert.ToString(dr["SupersedeMaterailDescription"]);
+        //                    SOI.Supersede.ValidFrom = Convert.ToDateTime(dr["ValidFrom"]);
+        //                    SOI.Supersede.ValidTo = Convert.ToDateTime(dr["ValidTo"]);
+        //                    SOI.Supersede.Description = Convert.ToString(dr["SupersedeRemarks"]);
+        //                    SOIs.Add(SOI);
+        //                }
+        //                return SOIs;
+        //                TraceLogger.Log(DateTime.Now);
+        //            }
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        new FileLogger().LogMessage("BDMS_Material", "GetMaterialSupersede", ex);
+        //        throw ex;
+        //    }
+        //    return SOIs;
+        //}
+         
 
         public List<PMaterialVariantType> GetMaterialVariantType(int? ProductTypeID)
         {
